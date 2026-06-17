e8e5de771cba1730638286b1c4898c5830c03ff1
mutiaming <mutiaradian07@gmail.com>
2026-06-17 21:57:57 +0700

    refactor config

--- backend/config.py
+++ backend/config.py
@@ -2,9 +2,27 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:@localhost/db_stunting')
+
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_USER = os.getenv('DB_USER', 'root')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+    DB_NAME = os.getenv('DB_NAME', 'db_stunting')
+
+    SQLALCHEMY_DATABASE_URI = (f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"  )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecret')
 
-    # ✅ Tambahkan baris ini:
-    ALLOWED_ORIGINS = os.getenv('ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
+    ALLOWED_ORIGINS = os.getenv(
+        'ALLOWED_ORIGINS',
+        'http://localhost:3000'
+    ).split(',')
+
+# class Config:
+#     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
+#     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:@localhost/db_stunting')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecret')
+
+#     # ✅ Tambahkan baris ini:
+#     ALLOWED_ORIGINS = os.getenv('ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
