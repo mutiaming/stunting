d248774c012d139539618895b5bd54484f4911e8
Moahmmad Rijal arfani <41555418+rizalarfani@users.noreply.github.com>
2026-06-18 06:25:01 +0700

    update cars domain backend

--- backend/config.py
+++ backend/config.py
@@ -15,7 +15,7 @@ class Config:
 
     ALLOWED_ORIGINS = os.getenv(
         'ALLOWED_ORIGINS',
-        'http://localhost:3000'
+        'https://growtrack.harkatnegeri.ac.id'
     ).split(',')
 
 # class Config:
